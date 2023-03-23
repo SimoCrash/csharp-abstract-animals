95e374153d26fa24b0c5d6346feba9d6614da16d
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-23 16:12:26 +0100

    Creazione classi

---
+++ Delfino.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Delfino : Animale
+{
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string? ToString()
+    {
+        return base.ToString();
+    }
+}
